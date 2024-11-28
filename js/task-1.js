feaa73b783cd8d0627fa3018ce62ff1ec7195d23
msslupin <aslinurgulnar005@gmail.com>
2024-11-29 01:46:51 +0300

    task-1 tamamlandı

--- js/task-1.js
+++ js/task-1.js
@@ -17,4 +17,4 @@ console.log(makeTransaction(5, 3000, 23000));
 console.log(makeTransaction(3, 1000, 15000)); 
 console.log(makeTransaction(10, 5000, 8000)); 
 console.log(makeTransaction(8, 2000, 10000)); 
-console.log(makeTransaction(10, 500, 5000)); 
+console.log(makeTransaction(10, 500, 5000));  
