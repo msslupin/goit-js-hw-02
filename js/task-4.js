8d5953538d619c532be1ae2cf6167469ed2642c9
msslupin <aslinurgulnar005@gmail.com>
2024-11-29 13:08:32 +0300

    js4

--- js/task-4.js
+++ js/task-4.js
@@ -21,10 +21,9 @@ function getShippingCost(country) {
   return `Shipping to ${country} will cost ${price} credits`;
 }
 
-
-console.log(getShippingCost("Australia")); 
-console.log(getShippingCost("Germany")); 
-console.log(getShippingCost("China")); 
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
 console.log(getShippingCost("Chile"));
-console.log(getShippingCost("Jamaica")); 
-console.log(getShippingCost("Sweden")); 
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
